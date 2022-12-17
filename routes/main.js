26527fc336cc9fc647051df4a152313672b2f71d
Konet <107544026+KonetOfficial@users.noreply.github.com>
2022-12-17 15:37:32 +0100

    Create main.js

---
+++ routes/main.js
@@ -0,0 +1,20 @@
+const client = require("../mongo/connect.js");
+const dbAnime = client.db("yami-anime").collection("anime-data")
+async function routes(fastify, options) {
+  fastify.get("/", async function (request, reply) {
+   const have = await dbAnime.find().toArray()
+
+      return reply.view('/pages/index.ejs', { animes: have })
+
+});
+  
+fastify.get("/watchtest", function (request, reply) {
+  reply.view("/pages/watch.html");
+});
+  fastify.get("/apropos", function (request, reply) {
+  reply.view("/pages/apropos.html");
+});
+
+
+}
+module.exports = routes;
