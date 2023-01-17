b2e34babbe8cc49fe6c7ab641a78a14988d575b7
Konet <107544026+KonetOfficial@users.noreply.github.com>
2023-01-17 12:41:55 +0100

    Update anime.js

--- routes/anime.js
+++ routes/anime.js
@@ -1,18 +1,39 @@
 const client = require("../mongo/connect.js");
-const dbAnime = client.db("yami-anime").collection("anime-data");
+const dbAnime = client.db("yami-anime").collection("anime-data")
 async function routes(fastify, options) {
   fastify.get("/", async function (request, reply) {
-   const have = await dbAnime.find().toArray();
-      return reply.view('/pages/index.ejs', { animes: have })
+   reply.redirect("/")
 });
-  
-fastify.get("/watchtest", function (request, reply) {
-  reply.view("/pages/watch.html");
-});
-  
-fastify.get("/apropos", function (request, reply) {
-  reply.view("/pages/apropos.html");
-});
-};
 
+fastify.get(`/:animeId`, async function (request, reply) {
+const animeId = request.params.animeId
+
+  return reply.redirect(`/animes/search?query=${animeId}`);
+});
+  
+fastify.get(`/info/:animeName`, async function (request, reply) {
+ const animeName = await request.params.animeName;
+
+  if (animeName) {
+  const have = await dbAnime.find({ title_url: animeName }).toArray();
+  const result = have[0]
+    if (have.length > 0) {
+      return reply.view('/pages/info.ejs', { anime: result })
+    } else {
+      return reply.view('/pages/index.ejs', { animes: have })
+    }
+  }  else {
+    return reply.send(`Merci d'indiquer:
+    /info/<nom>
+    
+    Ou retourner à l'accueil pour rechercher ou avoir toute la liste`)}
+});
+fastify.get("/new" , async function (request, reply) {
+ return reply.view("/pages/newanime.ejs");
+});
+fastify.post("/add" , async function (request, reply) {
+  await console.log(request.body)
+  return reply.send(request.body)
+});
+}
 module.exports = routes;
